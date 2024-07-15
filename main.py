39995c9a125ad9575550b563f88dac58d9c576d7
Xanion11 <krzysztof.j.ciesla@gmail.com>
2024-07-15 02:33:52 +0200

    kurwa dziala

---
+++ main.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+
+def kys():
+    print()
+
+
+if __name__ == "__main__":
+    print("chuj ci w dupe ")
+    root = tk.Tk()
+    kurwa = tk.Label(text="kurwa zajebie sie chyba zaraz")
+    kurwa.pack()
+    # frame = tk.Frame(root, bg="#0000ff")
+    # frame.pack()
+    # button = tk.Button(frame, bg="#ff0000", fg="#00ff00",
+    #                    text="chuj ci w dupe")
+    # button.place(frame)
+    root.mainloop()
