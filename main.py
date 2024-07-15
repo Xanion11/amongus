5df8bd54aa6c906e3b7c0507a9c996a09bd98e9f
Xanion11 <krzysztof.j.ciesla@gmail.com>
2024-07-15 12:26:01 +0200

    jprdl

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ def kys():
 if __name__ == "__main__":
     print("chuj ci w dupe ")
     root = tk.Tk()
-    kurwa = tk.Label(text="kurwa zajebie sie chyba zaraz")
-    kurwa.pack()
+    frame = tk.Frame()
+    kurwa = tk.Label(master=frame, text="kurwa zajebie sie chyba zaraz")
+    japierdole = tk.Button(master=frame, text="no kurwa zrobie to")
     # frame = tk.Frame(root, bg="#0000ff")
     # frame.pack()
     # button = tk.Button(frame, bg="#ff0000", fg="#00ff00",
     #                    text="chuj ci w dupe")
     # button.place(frame)
+    frame.pack()
+    kurwa.pack()
+    japierdole.pack()
+
     root.mainloop()
